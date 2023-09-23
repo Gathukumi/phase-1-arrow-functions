6aca3e8b01be9a940165b3135d8f554405e13b97
Gathukumi <gathukumidavid001@gmail.com>
2023-09-23 15:19:21 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,23 @@
+let num1 = 2000;
+let num2 = 100;
+
+function divide() {
+    return num1 / num2;
+};
+
+let result1 = divide();
+console.log(result1);  // Output: 20
+
+const square = (num) => {
+    return num * num;
+};
+const result2 = square(4); // Calling square with the argument 5
+console.log(result2); // Outputs: 25 (5 * 5)
+
+
+const add = (num3, num4)=> {
+    return num3 + num4;
+}
+const result3 = add(num3, num4);
+console.log =(result3)
+
